1b07e8483c3c046aa0e09611ead95b6344b93961
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-07-30 11:15:05 +0200

    Added ModelObjectExtensions class

---
+++ TeklaOpenAPIExtensionMethods/TeklaOpenAPIExtensionMethods/Model/ModelObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Tekla.Structures.Model;
+
+namespace TeklaOpenAPIExtension
+{
+    public static class ModelObjectExtensions
+    {
+        public static int GetIntReportProperty(this ModelObject modelObject, string name)
+        {
+            int value = int.MinValue;
+            modelObject.GetReportProperty(name, ref value);
+            return value;
+        }
+
+        public static double GetDoobleReportProperty(this ModelObject modelObject, string name)
+        {
+            double value = int.MinValue;
+            modelObject.GetReportProperty(name, ref value);
+            return value;
+        }
+
+        public static string GetStringReportProperty(this ModelObject modelObject, string name)
+        {
+            string value = string.Empty;
+            modelObject.GetReportProperty(name, ref value);
+            return value;
+        }
+    }
+}
