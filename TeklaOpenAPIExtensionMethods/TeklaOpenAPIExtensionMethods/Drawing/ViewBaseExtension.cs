c21ad2b46a506de577e62b30d12d6aa3b944edfc
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-08-06 22:07:10 +0200

    Added preprocesor directive to drawing methods. Added readme. Added license notice to every file.

--- TeklaOpenAPIExtensionMethods/TeklaOpenAPIExtensionMethods/Drawing/ViewBaseExtension.cs
+++ TeklaOpenAPIExtensionMethods/TeklaOpenAPIExtensionMethods/Drawing/ViewBaseExtension.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿/*
+MIT License
+
+Copyright (c) 2020 Dawid Dyrcz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+If you dont want to have codes which need reference to the Tekla.Structures.Drawing.dll then
+open properties of your project, goto Build > Conditional compilation symbols and add symbol NOT_TSD
+With NOT_TSD symbol the code bellow will not be included in your project
+*/
+
+#if !NOT_TSD
+
+using System;
 using System.Collections.Generic;
 using Tekla.Structures.Drawing;
 using Tekla.Structures.Geometry3d;
@@ -55,3 +87,4 @@ namespace TeklaOpenAPIExtension
         }
     }
 }
+#endif
---
+++ TeklaOpenAPIExtensionMethods/TeklaOpenAPIExtensionMethods/Readme.md
@@ -0,0 +1,11 @@
+﻿From version 2.2.0 I've added added preprocessor directive "NOT_TSD" to files which include drawing API. 
+I noticed that i dont need drawing api when i develop model plugin so from now this codes are optional.
+
+If you dont want to have codes which need reference to the Tekla.Structures.Drawing.dll then
+open properties of your project, goto "Build" > "Conditional compilation symbols" and add symbol "NOT_TSD"
+
+Official github repository:
+
+https://github.com/dawiddyrcz/Tekla-Open-Api-Extension-Methods
+
+You can contribute your own methods.
