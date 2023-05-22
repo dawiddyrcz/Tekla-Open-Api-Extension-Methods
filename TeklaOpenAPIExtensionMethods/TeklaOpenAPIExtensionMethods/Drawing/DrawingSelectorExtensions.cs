63d85f96922c3bd2c1f604848b8503556ab9c08d
Adam Griswold <adam.griswold@nucor.com>
2023-05-22 16:06:29 -0400

    feat(DrawingSelectorExtensions): Add extension method to get selected drawings from drawing selector

---
+++ TeklaOpenAPIExtensionMethods/TeklaOpenAPIExtensionMethods/Drawing/DrawingSelectorExtensions.cs
@@ -0,0 +1,53 @@
+﻿/*
+MIT License
+
+Copyright (c) 2022 Dawid Dyrcz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+If you dont want to have codes which need reference to the Tekla.Structures.Drawing.dll then
+open properties of your project, goto Build > Conditional compilation symbols and add symbol NOT_TSD
+With NOT_TSD symbol the code bellow will not be included in your project
+*/
+
+using System.Collections.Generic;
+using Tekla.Structures.Drawing.UI;
+
+#if !NOT_TSD
+
+namespace TeklaOpenAPIExtension.Drawing
+{
+	public static class DrawingSelectorExtensions
+	{
+		/// <summary>
+		/// Gets the drawings that are currently selected in the drawing list dialog of type <typeparamref name="TDrawing"/>.
+		/// </summary>
+		/// <typeparam name="TDrawing">Type of drawing to return.</typeparam>
+		/// <param name="selector">Selector to get all drawings from.</param>
+		/// <returns>The drawings that are currently selected in the drawing list dialog.</returns>
+		public static IReadOnlyCollection<TDrawing> GetSelected<TDrawing>(this DrawingSelector selector) where TDrawing : Tekla.Structures.Drawing.Drawing
+		{
+			return selector.GetSelected().ToReadOnlyCollection<TDrawing>();
+		}
+	}
+}
+
+#endif
